e65769ea5c7ff7b116b46a02bfbfb4f586ab2e40
Ravikumar, Abhiram (k20037424) <abhiram.ravikumar@kcl.ac.uk>
2021-09-24 15:54:27 +0100

    Replace hardcoded URL with variable

--- scripts/driver.py
+++ scripts/driver.py
@@ -5,11 +5,11 @@ import requests
 
 API_END_POINT_HELLO = "http://127.0.0.1:5000/helloworld" #Edit this URL as per need
 
-API_ENDPOINT_ECHO = "http://127.0.0.1:5000/echo" #Edit this URL as per need
+API_ENDPOINT_ECHO = "http://127.0.0.1:5000/echo?data=" #Edit this URL as per need
 
 def main():
     
-    #1.Call hello GET request
+    #1.Call helloworld GET request
     print("Fetching response from helloworld end-point...")    
     r = requests.get(url = API_END_POINT_HELLO)
     data = r.text
@@ -18,11 +18,12 @@ def main():
 
     #2.Call echo endpoint POST request
     print("Fetching response from echo end-point...")  
-    input_data_body = "Sun rises in the east."
-    URL = "http://127.0.0.1:5000/echo?data=" + input_data_body
+    input_data_body = "Sun rises in the east." #Change this input text body as per need.
+    URL = API_ENDPOINT_ECHO + input_data_body
     r = requests.post(URL)
     print("Data retrived from echo end-point:")
     print(r.text)
     print("=============================================================")
+
 if __name__=='__main__':
    main()
